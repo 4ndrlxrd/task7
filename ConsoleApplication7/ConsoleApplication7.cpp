410350710116d9ba187b2b5ce03d9b505f3dfa5d
4ndrlxrd <youvags@gmail.com>
2023-03-15 08:27:34 +0100

    first commit

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	int i = 1;
+	int sum = 0;
+	for (; i < 100; i = i + 1)
+	{
+		sum = sum + i;
+		//std::cout << (sum) << std::endl;
+		printf("%i+", i);
+	}
+	sum = sum + i;
+	printf("%i=%i", i,sum);
+	return 0;
+}
+//https://github.com/4ndrlxrd/task6.3
